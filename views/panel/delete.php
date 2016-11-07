4fad81d289532b44f0ff39d95820b470fc1c21eb
Mahdi Abedi <abedi@jeyserver.com>
2016-11-07 18:13:16 +0330

    fix error class name

--- views/panel/delete.php
+++ views/panel/delete.php
@@ -2,10 +2,10 @@
 namespace packages\contactus\views\panel;
 use \packages\contactus\view;
 use \packages\contactus\authorization;
-use \packages\contactus\contact_letter;
+use \packages\contactus\letter;
 class delete extends view{
 	protected $contactletter;
-	public function setLetter(contact_letter $letter){
+	public function setLetter(letter $letter){
 		$this->contactletter = $letter;
 	}
 	public function getLetter(){
