ad618b6a221cc680f8d5bc85efc52decbf3e35fa
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2016-11-17 00:45:55 +0330

    webuilder/userpanel#7: add a shortcut for contact us letters

---
+++ frontend/listeners/clipone/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+namespace themes\clipone\listeners\contactus;
+use \packages\base;
+use \packages\base\translator;
+use \packages\userpanel;
+use \packages\contactus\authorization;
+use \themes\clipone\views\dashboard as view;
+use \themes\clipone\views\dashboard\shortcut;
+class dashboard{
+	public function initialize(){
+		$this->addShortcuts();
+	}
+	protected function addShortcuts(){
+		if(authorization::is_accessed('list')){
+			$shortcut = new shortcut("contactus");
+			$shortcut->icon = 'fa fa-envelope';
+			$shortcut->color = shortcut::teal;
+			$shortcut->title = translator::trans('shortcut.contactus.title');
+			$shortcut->text = translator::trans('shortcut.contactus.text');
+			$shortcut->setLink(translator::trans('shortcut.contactus.link'), userpanel\url('contactus'));
+			view::addShortcut($shortcut);
+		}
+	}
+}
