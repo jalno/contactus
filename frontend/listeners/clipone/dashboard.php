81e856d5114c93423e340475de9f7d74181d4a50
Mahdi Abedi <abedi@jeyserver.com>
2018-10-26 10:27:23 +0330

    compatibility to webuilder/userpanel@247bf5e7b6c9c315f4a43448b7bdafb80e8edada

--- frontend/listeners/clipone/dashboard.php
+++ frontend/listeners/clipone/dashboard.php
@@ -1,24 +1,29 @@
 <?php
 namespace themes\clipone\listeners\contactus;
-use \packages\base;
-use \packages\base\translator;
-use \packages\userpanel;
-use \packages\contactus\authorization;
-use \themes\clipone\views\dashboard as view;
-use \themes\clipone\views\dashboard\shortcut;
+use packages\base\translator;
+use packages\userpanel;
+use packages\contactus\{authorization, letter};
+use themes\clipone\views\dashboard as view;
+use themes\clipone\views\dashboard\shortcut;
 class dashboard{
 	public function initialize(){
-		if(authorization::is_accessed('letter_search')){
+		if(authorization::is_accessed("letter_search")){
 			$this->addShortcuts();
 		}
 	}
 	protected function addShortcuts(){
+		$letter = new letter();
+		$letter->where("status", letter::answered, "!=");
+		$letters = $letter->count();
 		$shortcut = new shortcut("contactus");
-		$shortcut->icon = 'fa fa-envelope';
-		$shortcut->color = shortcut::teal;
-		$shortcut->title = translator::trans('shortcut.contactus.title');
-		$shortcut->text = translator::trans('shortcut.contactus.text');
-		$shortcut->setLink(translator::trans('shortcut.contactus.link'), userpanel\url('contactus'));
+		$shortcut->icon = "fa fa-envelope";
+		if ($letters) {
+			$shortcut->title = $letters;
+			$shortcut->text = translator::trans("shortcut.contactus.unread.letter");
+			$shortcut->setLink(translator::trans("shortcut.contactus.link"), userpanel\url("contactus"));
+		} else {
+			$shortcut->text = translator::trans("shortcut.contactus.unread.letter.iszero");
+		}
 		view::addShortcut($shortcut);
 	}
 }
